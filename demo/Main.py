5580ee6baf5350520591f8d35f50163c26cabe0d
Philip Goldman <philip.goldm@gmail.com>
2018-03-25 09:12:13 +0300

    Added comments in main file

--- demo/Main.py
+++ demo/Main.py
@@ -1,15 +1,18 @@
 import random
-
+from demo.message import Message, MessageFactory
 # http://python-3-patterns-idioms-test.readthedocs.io/en/latest/Factory.html#exercises
 # https://stackoverflow.com/questions/3862310/how-to-find-all-the-subclasses-of-a-class-given-its-name
-from demo.message import Message, MessageFactory
+
 
 def message_generator(n):
     '''
     Generate random messages.
     n - count of messages
     '''
+
+    '''Get available message types'''
     types = Message.Message.__subclasses__()
+
     for i in range(n):
        yield random.choice(types).__name__
 
